42a8fb5f0e3513e95f8d8129cab93f40d2a6c067
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-04-20 16:44:08 +0000

    fix test

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ ALIVE_CHAINER_JP="$(perl -ne 'print $1 if m|;url=(.+?)"|' docs/chainer-jp.html)"
 EXPIRED_CHAINER="https://chainer.slack.com/join/shared_invite/enQtMjk4ODE3NjA4MzM3LTNiMzM4YmY3ZTFjMmE3YWZlY2IzMTg3NGZiMzE2OGExMTExYTcxNzRjNGM3Mjc4ZjY3MjZlOTNlNzZmMTE2M2M"
 EXPIRED_CHAINER_JP="https://chainer-jp.slack.com/join/shared_invite/enQtMjk5MzU0MTQ4ODM2LWUyOThkNjMwZTcwZDY5ZTg5YmY1NWYxOGZhZTk4ZmQyNjQxNTc2NjVmNGFjMzI5MjlhMDllZGI4MzRiZTMwODI"
 
-KEY="terms_of_service.css"
+KEY="Join chainer"
 
 # Expect a key that only appears in alive invitation links.
 ( curl -L "${EXPIRED_CHAINER}"    | grep -F "${KEY}" ) && exit 1
